f620d44ca9e4356ad9a817ab05535e2e337860b3
jongyun08 <ljyoon0807@naver.com>
2024-09-03 20:25:07 +0900

    KillZone 추가

--- Source/HrizShtPrj/private/KillZone1.cpp
+++ Source/HrizShtPrj/private/KillZone1.cpp
@@ -9,7 +9,7 @@
 AKillZone1::AKillZone1()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// PrimaryActorTick.bCanEverTick = true;
 
 	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("KillZone1"));
 	SetRootComponent(boxComp);
--- Source/HrizShtPrj/private/KillZone3.cpp
+++ Source/HrizShtPrj/private/KillZone3.cpp
@@ -1,33 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "KillZone2.h"
+#include "KillZone3.h"
 #include "Components/BoxComponent.h"
 
 // Sets default values
-AKillZone2::AKillZone2()
+AKillZone3::AKillZone3()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	// PrimaryActorTick.bCanEverTick = true;
 
-	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("KillZone2"));
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("KillZone3"));
 	SetRootComponent(boxComp);
 	boxComp->SetMobility(EComponentMobility::Static);
 
 	boxComp->SetBoxExtent(FVector(50.f, 2000.f, 50.f));
 
-	boxComp->SetCollisionProfileName(TEXT("KillZone2"));
+	boxComp->SetCollisionProfileName(TEXT("KillZone3"));
 }
 
 // Called when the game starts or when spawned
-void AKillZone2::BeginPlay()
+void AKillZone3::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void AKillZone2::Tick(float DeltaTime)
+void AKillZone3::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
